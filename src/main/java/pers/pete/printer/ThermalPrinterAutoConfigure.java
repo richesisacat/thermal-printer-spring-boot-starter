6829048bb67d99b694dd882b60732c81531e3ac4
Yang.Yu <Yang.Yu@123.com>
2019-07-18 10:30:51 +0800

    fix autoconfigure bean name

--- src/main/java/pers/pete/printer/ThermalPrinterAutoConfigure.java
+++ src/main/java/pers/pete/printer/ThermalPrinterAutoConfigure.java
@@ -1,12 +1,12 @@
 package pers.pete.printer;
 
-import pers.pete.printer.enums.PaperWidth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pers.pete.printer.enums.PaperWidth;
 
 @Configuration
 @ConditionalOnClass(ThermalPrinter.class)
@@ -22,7 +22,7 @@ public class ThermalPrinterAutoConfigure {
 
   @Bean
   @ConditionalOnMissingBean
-  public ThermalPrinter init() {
+  public ThermalPrinter thermalPrinter() {
     if (PaperWidth.USERDEFINED.equals(properties.getPaperWidth())) {
       if (null == properties.getUserDefinedWidth()) {
         throw new RuntimeException("properties file userDefinedWidth is null!");
